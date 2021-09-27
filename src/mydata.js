9585032c8c38b8a8663a2fc2064b5a2d5e9c45c2
Yauhen Davidovich <davidovich336@gmail.com>
2021-09-27 10:58:13 +0200

    feat: add projects

--- src/mydata.js
+++ src/mydata.js
@@ -10,7 +10,10 @@ import myPhoto from "./images/my-photo.jpg"
 //Progect Covers
 import fancyWeather from "./images/fancy-weather-cover.jpg"
 import englishForKids from "./images/english-for-kids.jpg"
-
+import socialNetwork from "./images/social-network-cover.png"
+import freedomRunner from "./images/freedom-runner.jpg"
+import covidDashboard from "./images/covid-dashboard.jpg"
+import cards from "./images/cards.jpg"
 
 
 // Social Icon
@@ -18,157 +21,160 @@ import githubIcon from "./images/github.svg"
 import linkedinIcon from "./images/linkedin.svg"
 
 export default {
-  //   Header Details ---------------------
-  name: "Yauhen",
-  headerTagline: [
-    //Line 1 For Header
-    "Yauhen Davidovich.",
-    //Line 2 For Header
-    "I build things for the web."
-  ],
-  //   Header Paragraph
-  headerParagraph:
-    "Front End Developer ",
+    //   Header Details ---------------------
+    name: "Yauhen",
+    headerTagline: [
+        //Line 1 For Header
+        "Yauhen Davidovich.",
+        //Line 2 For Header
+        "I build things for the web."
+    ],
+    //   Header Paragraph
+    headerParagraph:
+        "Front End Developer ",
 
-  //Contact Email
-  contactEmail: "davidovichyauhen@gmail.com",
+    //Contact Email
+    contactEmail: "davidovichyauhen@gmail.com",
 
-  // End Header Details -----------------------
+    // End Header Details -----------------------
 
-  // Work Section ------------------------
-  projects: [
-    {
-      title: "Project One",
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+    // Work Section ------------------------
+    projects: [
+        {
+            title: "Social network",
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
 
-      imageSrc:
-        "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+            imageSrc:
+            socialNetwork,
 
-      url: "http://test.com/",
-    },
-    {
-      title: "Project Two",
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+            url: "https://github.com/YauhenDavidovich/Social-network/tree/develop",
+            page: "https://github.com/YauhenDavidovich/Social-network/tree/develop",
+        },
+        {
+            title: "Cards",
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+            //Project Image - Add Your Project Image Here
+            imageSrc:
+            cards,
+            url: "https://github.com/YauhenDavidovich/cards",
+            page: "https://yauhendavidovich.github.io/cards",
+        },
+        {
+            title: "Fancy Weather",
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+            imageSrc:
+            fancyWeather,
+            url: "http://test.com/",
+        },
+        {
+            title: "English for kids",
+            para:
+                "App for learning English words by children.",
+            imageSrc:
+            englishForKids,
+            url: "https://github.com/YauhenDavidovich/rsschool2020-2021/tree/english-for-kids/english-for-kids",
+            page: "https://yauhendavidovich-english-for-kids.netlify.app/",
+        },
+        {
+            title: "Freedom Runner",
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
 
-      imageSrc:
-        "https://images.unsplash.com/photo-1605153864431-a2795a1b2f95?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNTY3ODl8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=60",
+            imageSrc:
+            freedomRunner,
+            url: "https://github.com/YauhenDavidovich/rsclone/tree/develop",
+            page: "https://freedom-runner-rsschool2020q3-yauhen-davidovich.netlify.app/",
+        },
+        {
+            title: "Covid Dashboard",
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
 
-      url: "http://test.com/",
-    },
-    {
-      title: "Fancy Weather",
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
-      imageSrc:
-          fancyWeather,
-      url: "http://test.com/",
-    },
-    {
-      title: "English for kids",
-      para:
-        "App for learning English words by children.",
-      imageSrc:
-      englishForKids,
-      url: "https://github.com/YauhenDavidovich/rsschool2020-2021/tree/english-for-kids/english-for-kids",
-      page: "https://yauhendavidovich-english-for-kids.netlify.app/",
-    },
-    {
-      title: "Project Five",
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+            imageSrc:
+            covidDashboard,
+            url: "https://github.com/nkrivitsky/COVID-19-Dashboard/tree/develop",
+            page: "https://covid-dashboard-yauhendavidovich-nkrivitsky-omaslov.netlify.app/"
+        },
 
-      imageSrc:
-        "https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
-      //Project URL - Add Your Project Url Here
-      url: "http://test.com/",
-    },
-    {
-      title: "Project Six",
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
-      //Project Image - Add Your Project Image Here
-      imageSrc:
-        "https://images.unsplash.com/photo-1517479149777-5f3b1511d5ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwYXN0ZWx8ZW58MHx8MHw%3D&auto=format&fit=crop&w=400&q=60",
+    ],
 
-      url: "http://test.com/",
-    },
+    // End Work Section -----------------------
 
-  ],
+    // About Secton --------------
+    aboutParaOne:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    aboutParaTwo:
+        "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    aboutParaThree:
+        "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    aboutImage: myPhoto,
 
-  // End Work Section -----------------------
+    //   End About Section ---------------------
 
-  // About Secton --------------
-  aboutParaOne:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-  aboutParaTwo:
-    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  aboutParaThree:
-    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-  aboutImage: myPhoto,
+    // Skills Section ---------------
 
-  //   End About Section ---------------------
+    //   Import Icons from the top and link it here
 
-  // Skills Section ---------------
+    skills: [
+        {
+            img: htmlIcon,
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        },
+        {
+            img: cssIcon,
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        },
+        {
+            img: jsIcon,
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        },
+        {
+            img: reactIcon,
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        },
+        {
+            img: testingIcon,
+            para:
+                "Unit Tests, Storybook",
+        },
+        {
+            img: codeIcon,
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        },
+    ],
 
-  //   Import Icons from the top and link it here
+    // End Skills Section --------------------------
 
-  skills: [
-    {
-      img: htmlIcon,
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-    },
-    {
-      img: cssIcon,
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-    },
-    {
-      img: jsIcon,
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-    },
-    {
-      img: reactIcon,
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-    },
-    {
-      img: testingIcon,
-      para:
-        "Unit Tests, Storybook",
-    },
-    {
-      img: codeIcon,
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-    },
-  ],
+    //   Promotion Section --------------------------
 
-  // End Skills Section --------------------------
+    promotionHeading: "Heading",
+    promotionPara:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    // End Promotion Section -----------------
 
-  //   Promotion Section --------------------------
+    //   Contact Section --------------
 
-  promotionHeading: "Heading",
-  promotionPara:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  // End Promotion Section -----------------
+    contactSubHeading: "Let's create your next experience together",
+    social: [
+        // Add Or Remove The Link Accordingly
+        {
+            img: githubIcon,
+            url: "https://github.com/yauhendavidovich"
+        },
+        {
+            img: linkedinIcon,
+            url: "https://www.linkedin.com/in/yauhendavidovich/",
+        },
+    ],
 
-  //   Contact Section --------------
-
-  contactSubHeading: "Let's create your next experience together",
-  social: [
-    // Add Or Remove The Link Accordingly
-    { img: githubIcon,
-      url: "https://github.com/yauhendavidovich" },
-    {
-      img: linkedinIcon,
-      url: "https://www.linkedin.com/in/yauhendavidovich/",
-    },
-  ],
-
-  // End Contact Section ---------------
+    // End Contact Section ---------------
 }
 
