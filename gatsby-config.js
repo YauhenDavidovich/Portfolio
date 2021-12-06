9285596741e8f6c576efe0af3a0cb0c256c9d070
Yauhen Davidovich <davidovich336@gmail.com>
2021-12-06 21:17:10 +0100

    feat: update tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
                 // The property ID; the tracking code won't be generated without it
-                trackingId: "G-SG8HPHZXBG",
+                trackingId: "UA-214475156-1",
                 // Defines where to place the tracking script - `true` in the head and `false` in the body
                 head: false,
                 // Setting this parameter is optional
